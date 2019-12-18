b9c921ce7797c28caa1f2fb965bd6425676f95aa
Andrei Balescu <andrei.balescu@gmail.com>
2019-12-18 05:30:41 -0800

    #5 hello world middleware

--- pluralsight.com/ASP.NET Core Fundamentals by Scott Allen/OdeToFood/OdeToFood/Startup.cs
+++ pluralsight.com/ASP.NET Core Fundamentals by Scott Allen/OdeToFood/OdeToFood/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -38,25 +39,39 @@ namespace OdeToFood
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configures middleware to be run on every request/response in the pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                // only concerned with responses
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // forces use of HTTPS
                 app.UseHsts();
             }
 
+            app.Use(SayHelloMiddleware);
+
             app.UseHttpsRedirection();
+            // serves files from wwwroot folder
+            // can take in a `StaticFileOptions` that configures its behavior
             app.UseStaticFiles();
+
+            // Enables GDPR support: https://docs.microsoft.com/en-us/aspnet/core/security/gdpr?view=aspnetcore-3.1
+            // not present by default in .NET Core 3.1
+            // app.UseCookiePolicy();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // enables websocket communication
+            // app.UseSignalR();
 
             app.UseEndpoints(endpoints =>
             {
@@ -65,6 +80,25 @@ namespace OdeToFood
                 // enables routing to API controllers
                 endpoints.MapControllers();
             });
+        }
+
+        private RequestDelegate SayHelloMiddleware(RequestDelegate nextMiddlewareInPipeline)
+        {
+            RequestDelegate helloWorldResponder = async context => 
+            {
+                const string c_helloWorldEndpoint = "/hello";
+                bool isHelloWorldEndpoint = string.Equals(c_helloWorldEndpoint, context.Request.Path, StringComparison.InvariantCultureIgnoreCase);
+                if (isHelloWorldEndpoint)
+                {
+                    await context.Response.WriteAsync("Hello, World!");
+                }
+                else
+                {
+                    await nextMiddlewareInPipeline(context);
+                }
+            };
+
+            return helloWorldResponder;
         }
     }
 }
