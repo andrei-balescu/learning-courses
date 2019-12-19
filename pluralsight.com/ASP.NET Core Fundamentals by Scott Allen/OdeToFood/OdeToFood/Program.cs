2af813afe180777a7c093c0c08e78ed18115c601
Andrei Balescu <andrei.balescu@gmail.com>
2019-12-19 05:03:42 -0800

    #5 application execution of EF migrations

--- pluralsight.com/ASP.NET Core Fundamentals by Scott Allen/OdeToFood/OdeToFood/Program.cs
+++ pluralsight.com/ASP.NET Core Fundamentals by Scott Allen/OdeToFood/OdeToFood/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -14,7 +17,22 @@ namespace OdeToFood
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            MigrateDatabase(host);
+
+            host.Run();
+        }
+
+        // The application needs the dbcreator role (SQL Server) to execute EF migrations
+        private static void MigrateDatabase(IHost host)
+        {
+            // create scope to get services using dependency injection
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+                dbContext.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
