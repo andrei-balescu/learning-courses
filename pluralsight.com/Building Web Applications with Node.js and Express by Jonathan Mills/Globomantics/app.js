18536e1497514774d8975f88722dac0ec7a3f384
Andrei Bălescu <andrei.balescu@gmail.com>
2022-10-12 16:09:57 +0300

    Passing data to page (#16)

--- pluralsight.com/Building Web Applications with Node.js and Express by Jonathan Mills/Globomantics/app.js
+++ pluralsight.com/Building Web Applications with Node.js and Express by Jonathan Mills/Globomantics/app.js
@@ -12,10 +12,11 @@ const morgan = require('morgan');
 // manage local file paths
 // part of Node.js package
 const path = require('path');
+const app = express();
+const sessionRouter = express.Router();
 
 const PORT = process.env.PORT || 3000;
-const app = express();
-const sessionsRouter = express.Router();
+const sessionData = require('./src/data/sessions.json');
 
 // app.use(morgan('combined'));
 app.use(morgan('tiny'));
@@ -26,24 +27,19 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-sessionsRouter.route('/')
+sessionRouter.route('/')
     .get((request, response) => {
         response.render('sessions', {
-            sessions: [
-                { title: 'Session 1', description: 'This is session 1' },
-                { title: 'Session 2', description: 'This is session 2' },
-                { title: 'Session 3', description: 'This is session 3' },
-                { title: 'Session 4', description: 'This is session 4' }
-            ]
+            sessions: sessionData
         });
     });
 
-sessionsRouter.route('/1')
+sessionRouter.route('/1')
     .get((request, response) => {
         response.send('hello session 1');
     });
 
-app.use('/sessions', sessionsRouter);
+app.use('/sessions', sessionRouter);
 
 app.get('/', (request, response) => {
     response.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] })
