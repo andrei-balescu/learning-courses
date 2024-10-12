ea155357bb5db87fac1cc60d489abf1b9469704e
Andrei Bălescu <andrei.balescu@gmail.com>
2024-10-12 14:58:51 +0300

    Sanitized DB data (#24)

--- youtube.com/SQL Development Made Easy with Docker by Tim Corey/AdventureWorksDB/backup/DB_AW_RESTORE_SANITIZE.sql
+++ youtube.com/SQL Development Made Easy with Docker by Tim Corey/AdventureWorksDB/backup/DB_AW_RESTORE_SANITIZE.sql
@@ -4,3 +4,14 @@ MOVE 'AdventureWorksLT2022_Data' TO '/var/opt/mssql/data/AdventureWorks.mdf',
 MOVE 'AdventureWorksLT2022_Log' TO '/var/opt/mssql/data/AdventureWorks.ldf',
 NOUNLOAD, REPLACE, STATS = 5
 GO
+
+-- Sanitize DB
+USE AdventureWorks
+GO
+
+UPDATE SalesLT.Customer
+SET EmailAddress = FirstName + 'example.com',
+    LastName = 'Customer',
+    Phone = '000-000-0000',
+    PasswordHash = '',
+    PasswordSalt = '';
