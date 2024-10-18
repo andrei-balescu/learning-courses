4fb16294e4686bae7d211b635a0140b9a74ca2dc
Andrei Bălescu <andrei.balescu@gmail.com>
2024-10-18 13:24:24 +0300

    Executting mysql triggers from command line (#26)

--- freecodecamp.org/SQL Tutorial by Mike Dane/mssql/13. TRIGGERS.sql
+++ freecodecamp.org/SQL Tutorial by Mike Dane/mssql/13. TRIGGERS.sql
@@ -17,18 +17,20 @@ BEGIN
         ELSE CONCAT('Added other employee: ', FirstName, ' ', LastName)
     END
     FROM INSERTED;
+    INSERT INTO TriggerTest VALUES ('Batch completed.');
 END;
 GO
 
 INSERT INTO Employee VALUES 
 	(109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3),
-    (110, 'Sarah', 'Mallone', '1978-02-19', 'F', 69000, 106, 3),
-    (111, 'Max', '', '1978-02-19', 'A', 1000, 106, 3);
+    (110, 'Kevin', 'Mallone', '1978-02-19', 'M', 69000, 106, 3),
+    (111, 'Pam', 'Beesly', '1988-02-19', 'F', 69000, 106, 3),
+    (112, 'Max', '', '2024-02-19', 'A', 1000, 106, 3);
 
 SELECT * FROM TriggerTest;
 
 DELETE FROM Employee
-WHERE EmpID IN (109, 110, 111);
+WHERE EmpID IN (109, 110, 111, 112);
 
 DROP TRIGGER TR_Employee_INSERT;
 DROP TABLE TriggerTest;
--- freecodecamp.org/SQL Tutorial by Mike Dane/mysql/13. TRIGGERS.sql
+++ freecodecamp.org/SQL Tutorial by Mike Dane/mysql/13. TRIGGERS.sql
@@ -4,26 +4,40 @@ CREATE TABLE trigger_test(
 	message VARCHAR(100)
 );
 
--- DELIMITER $$ -- replaces semicolon(;) default statement delimiter - used inside trigger code;
--- only works from mysql command line, NOT in editor window: https://stackoverflow.com/questions/10259504/delimiters-in-mysql
 CREATE TRIGGER tr_employee_insert
 AFTER INSERT
 ON employee
 FOR EACH ROW 
--- BEGIN
 	INSERT INTO trigger_test VALUES (CONCAT('added new employee: ', NEW.first_name, ' ', NEW.last_name));
--- END$$
--- DELIMITER ;
+
+DELIMITER $$ -- replaces semicolon(;) statement delimiter - used inside trigger code;
+-- only works from mysql command line, NOT in editor window (ignored): https://stackoverflow.com/questions/10259504/delimiters-in-mysql
+-- see docker-compose.yaml for instructions.
+CREATE TRIGGER tr_employee_insert2
+AFTER INSERT
+ON employee
+FOR EACH ROW BEGIN
+	IF NEW.sex = 'M' THEN
+		INSERT INTO trigger_test VALUES ('added male employee');
+	ELSEIF NEW.sex = 'F' THEN
+		INSERT INTO trigger_test VALUES ('added female employee');
+	ELSE
+		INSERT INTO trigger_test VALUES ('added other employee');
+	END IF;
+END$$
+DELIMITER ;
+
 SHOW TRIGGERS;
 
 INSERT INTO employee VALUES 
 	(109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3),
-    (110, 'Kevin', 'Mallone', '1978-02-19', 'M', 69000, 106, 3);
+    (110, 'Kevin', 'Mallone', '1978-02-19', 'M', 69000, 106, 3),
+    (111, 'Pam', 'Beesly', '1988-02-19', 'F', 69000, 106, 3);
 
 SELECT * FROM trigger_test;
 
 DELETE FROM employee
-WHERE emp_id IN (109, 110);
+WHERE emp_id IN (109, 110, 111);
 
 DROP TRIGGER tr_employee_insert;
 DROP TABLE trigger_test;
