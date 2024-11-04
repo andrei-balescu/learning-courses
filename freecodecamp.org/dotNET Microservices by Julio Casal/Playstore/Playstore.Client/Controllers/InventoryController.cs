27e2fb92be683d6e4e4734864bba53b0d79b8a54
Andrei Bălescu <andrei.balescu@gmail.com>
2024-11-04 12:52:10 +0200

    List catalog items for purchase (#30)

--- freecodecamp.org/dotNET Microservices by Julio Casal/Playstore/Playstore.Client/Controllers/InventoryController.cs
+++ freecodecamp.org/dotNET Microservices by Julio Casal/Playstore/Playstore.Client/Controllers/InventoryController.cs
@@ -1,5 +1,6 @@
 using MassTransit.Initializers;
 using Microsoft.AspNetCore.Mvc;
+using Playstore.Client.Models.Catalog;
 using Playstore.Client.Models.Inventory;
 using Playstore.Client.ServiceClients;
 
@@ -8,6 +9,9 @@ namespace Playstore.Client.Controllers;
 /// <summary>Operations with inventory items.</summary>
 public class InventoryController : Controller
 {
+    /// <summary>Client for communicating with the catalog service.</summary>
+    private readonly ICatalogClient _catalogClient;
+
     /// <summary>Client for communicating with the inventory service.</summary>
     private readonly IInventoryClient _inventoryClient;
 
@@ -15,9 +19,10 @@ public class InventoryController : Controller
     /// <remarks>Hardcoded until user/identity service available.</remarks>
     private  Guid _hardcodedUserId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6");
 
-    public InventoryController(IInventoryClient inventoryClient)
+    public InventoryController(IInventoryClient inventoryClient, ICatalogClient catalogClient)
     {
         _inventoryClient = inventoryClient;
+        _catalogClient = catalogClient;
     }
 
     /// <summary>Lists the items in the inventory.</summary>
@@ -34,5 +39,21 @@ public class InventoryController : Controller
             });
 
         return View(items);
+    }
+
+    /// <summary>Lists catalog items for purchase.</summary>
+    /// <returns>Inventory/Purchase page.</returns>
+    public async Task<IActionResult> Purchase()
+    {
+        IEnumerable<CatalogItemViewModel> catalogItems = (await _catalogClient.GetAllItemsAsync())
+            .Select(dto => new CatalogItemViewModel
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price
+            });
+
+        return View(catalogItems);
     }
 }
